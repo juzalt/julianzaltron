3bc290aeab051022f27d003055497e640520e642
zxltrn <zaltron.julian@gmail.com>
2019-09-26 08:53:25 -0300

    Header shadow

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,15 +4,17 @@ import Header from '../Header';
 
 function Home(props){
   return(
-    <div id="container">
+    <>
       <Header/>
-      <p className="hero_h3">Julian Zaltron is a...</p>
-      <p className="hero_p1">Software engineer,</p>
-      <p className="hero_p2">compsci student,</p>
-      <p className="hero_p3">juggler...</p>
-      <p className="hero_p4">...or just someone who loves to try new things.</p>
-      {props.children}
-    </div>
+      <div id="container">
+        <p className="hero_h3">Julian Zaltron is a...</p>
+        <p className="hero_p1">Software engineer,</p>
+        <p className="hero_p2">compsci student,</p>
+        <p className="hero_p3">juggler...</p>
+        <p className="hero_p4">...or just someone who loves <br /> to try out new things.</p>
+        {props.children}
+      </div>
+    </>
   );
 }
 
