3b7427ca47a76022404e3fc22f1cd9cbe034231f
zxltrn <zaltron.julian@gmail.com>
2019-09-30 17:11:18 -0300

    Continued work on hero

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './style.css'
 import Header from '../Header';
+import Octocat from './Octocat.png';
 
 function Home(props){
   return(
     <>
       <Header/>
       <div id="container">
-        <p className="hero_h3">Julian Zaltron is a...</p>
-        <p className="hero_p1">Software engineer,</p>
-        <p className="hero_p2">compsci student,</p>
-        <p className="hero_p3">juggler...</p>
-        <p className="hero_p4">...or just someone who loves <br /> to try out new things.</p>
-        {props.children}
-      </div>
+        <p className="hero_h3">Welcome!</p>
+        <p className="hero_p1">Under construction.</p>
+        <p className="hero_p4">Still here? <br /> while waiting for the page to be finished, you can checkout my stuff right here:</p>
+        <a href="github.com/juzalt"><img src={Octocat} alt="Githubs octocat logo" className="octocat"/></a>
+     </div>
     </>
   );
 }
