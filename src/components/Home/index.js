c4a9a4bd1b5b71e99d631d11fe7cabfb330ea087
zxltrn <zaltron.julian@gmail.com>
2019-10-06 19:52:44 -0300

    *

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,18 +8,20 @@ import SideBar from '../SideBar';
 function Home(){
   return(
     <>
-      <Header/>
-      <div id="absContainer">
-        <SideBar/>
-        <div id="container">
-          <p className="hero_h3">Welcome! This site is...</p>
-          <p className="hero_p1">Under construction.</p>
-          <p className="hero_p4">Still here? <br /> while waiting for the page to be finished, <br/> you can checkout my stuff right here:</p>
-          <SocialNetworks/>
-          <p className="hero_p5">Or feel free to reach out :)</p>
+      <div id="fullPageContainer">
+        <Header/>
+        <div id="absContainer">
+          <SideBar/>
+          <div id="container">
+            <p className="hero_h3">Welcome! This site is...</p>
+            <p className="hero_p1">Under construction.</p>
+            <p className="hero_p4">Still here? <br /> while waiting for the page to be finished, <br/> you can checkout my stuff right here:</p>
+            <SocialNetworks/>
+            <p className="hero_p5">Or feel free to reach out :)</p>
+          </div>
         </div>
+        <Footer/>
       </div>
-      <Footer/>
     </>
   );
 }
