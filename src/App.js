3c18d75e2012d579ee40f2895665cae7bb8cec92
zxltrn <zaltron.julian@gmail.com>
2019-10-06 00:20:48 -0300

    Added favicon and position hero differently

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
-import Header from './components/Header';
 import Home from './components/Home';
 
 function App() {
