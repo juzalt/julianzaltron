3cabb721ba11aab8998f99b4497776843ed18ffb
Julian Zaltron <jzaltron@k12.com>
2021-08-26 18:12:46 -0300

    easter egg

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css';
 import Logo from './logozaltron.svg';
 import { Link } from 'react-router-dom'; 
-import GmailLogo from 'gmailLogo.png';
+import GmailLogo from './gmailLogo.png';
 
 function Header(props){
   return(
@@ -10,9 +10,9 @@ function Header(props){
       <div className="navContainer">
         <Link to="/"><img src={Logo} alt="Zaltrons logo" className="logo"/></Link>
         <div className="aboutMe headerElement">
-          <p>🖤</p>
+          <span role="img" aria-label="Black heart emoji. Click on it for a surprise." ><a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">🖤</a></span>
         </div>
-        <a class="headerElement" target="_blank" rel="noopener" href="mailto:zaltron.julian@gmail.com"><img src={GmailLogo} alt="Gmail logo" className="gmail"/></a>
+        <a class="headerElement" target="_blank" rel="noopener noreferrer" href="mailto:zaltron.julian@gmail.com"><img src={GmailLogo} alt="Gmail logo" className="gmail"/></a>
       </div>
     </>
   );
