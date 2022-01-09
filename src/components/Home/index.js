ab168695bd71119f6082ee64e33a18694e54ec5c
Julian Zaltron <jzaltron@k12.com>
2022-01-09 02:04:20 -0300

    draw method

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,15 +8,19 @@ function Home(){
   let _canvas;
 
   useEffect(() => {
+  }, []);
+  
+  draw(() =>  {
     _canvas = document.getElementById("clouds");
     var ctx = _canvas.getContext("2d");
     ctx.rotate(Math.random() * 2 * Math.PI);
     ctx.fillStyle = "rgb(200,0,0)";
     ctx.fillRect (10, 10, 55, 50);
-
+  
     ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
     ctx.fillRect (30, 30, 55, 50);
-  }); 
+
+  })
 
   return(
       <div id="fullPageContainer">
