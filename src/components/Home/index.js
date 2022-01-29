86a5ed58307d6942192099356c7ffb4364484706
Julian Zaltron <jzaltron@k12.com>
2022-01-29 02:11:04 -0300

    threejs installed. draw method

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import './style.css'
 import Header from '../Header';
 
 
 function Home(){
+  const [drawedCanvas, draw] = useState(0);
   let _canvas;
 
   useEffect(() => {
+      draw(() =>  {
+        _canvas = document.getElementById("clouds");
+        var ctx = _canvas.getContext("2d");
+        ctx.rotate(Math.random() * 2 * Math.PI);
+        ctx.fillStyle = "rgb(200,0,0)";
+        ctx.fillRect (10, 10, 55, 50);
+      
+        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+        ctx.fillRect (30, 30, 55, 50);
+    });
+
   }, []);
   
-  draw(() =>  {
-    _canvas = document.getElementById("clouds");
-    var ctx = _canvas.getContext("2d");
-    ctx.rotate(Math.random() * 2 * Math.PI);
-    ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect (10, 10, 55, 50);
-  
-    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    ctx.fillRect (30, 30, 55, 50);
-
-  })
 
   return(
       <div id="fullPageContainer">
