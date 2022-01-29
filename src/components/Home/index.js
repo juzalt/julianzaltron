e618c9d4692e6a2e4216ac4dbf1cbea72b305d4f
Julian Zaltron <jzaltron@k12.com>
2022-01-29 02:29:58 -0300

    gif, css props

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,28 +5,13 @@ import Header from '../Header';
 
 
 function Home(){
-  let _canvas;
-
   useEffect(() => {
   }, []);
-  
-  draw(() =>  {
-    _canvas = document.getElementById("clouds");
-    var ctx = _canvas.getContext("2d");
-    ctx.rotate(Math.random() * 2 * Math.PI);
-    ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect (10, 10, 55, 50);
-  
-    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    ctx.fillRect (30, 30, 55, 50);
-
-  })
 
   return(
       <div id="fullPageContainer">
         <Header/>
-        <canvas width="9999" height="9999" id="clouds" style={{ "position": "absolute" }}></canvas>
-        <div role="main" style={{ 'backgroundImage': _canvas ? "url(" + _canvas.toDataURL() + ")" : "" }}>
+        <div role="main" className='main'>
           <div id="heroContainer">
             <div id="heroCenterContainer">
               <p className="hero_h3">Hi, my name is</p>
